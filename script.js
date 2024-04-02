f3ba5ce979ce9896cb329be2eb58b9f4f11f3589
RobertsAlexis <lexi.g.roberts@gmail.com>
2024-04-01 22:26:56 -0500

    the fungus defeated me

--- script.js
+++ script.js
@@ -3,12 +3,40 @@
 // Example:
 // let fungusHP = 100;
 
+// Starting game
+let attackPoints = 100;
+let hitPoints = 100;
+
+
+// actions 
+function arcaneScepter(event){
+    let attackPoints = apCost -12;
+    let hitPoints = hpDamage -14;
+   // arcaneScepter = document.getElementByClassId('attack-btn arcane-scepter');
+
+};
+function entangle(event){ 
+    let attackPoints = apCost -23;
+    let hitPoints = hpDamage -9;
+  //  entangle = document.getElementByClassId('attack-btn entangle');
+};
+function dragonBlade(){
+   let attackPoints = apCost -38;
+   let hitPoints = hpDamage -47;
+  // dragonBlade= document.getElementByClassId('attack-btn dragon-blade');
+};
+function starFire(event){
+    let attackPoints = apCost -33;
+    let hitPoints = hpDamage -25;
+  //  starFire = document.getElementByClassID('attack-btn star-fire');
+ };
+
+
 function onReady() {
     console.log("Ready to go!")
-    
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
+   
     
     // 🧠 Remember
     // - Handle events that ->
